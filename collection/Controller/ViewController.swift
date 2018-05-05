4a71f9b815837fb7c48bc8ef544633dd8001f381
Kojima <canonn301@gmail.com>
2018-05-05 20:30:59 +0900

    add Auth and Storage.

--- collection/Controller/ViewController.swift
+++ collection/Controller/ViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
-
+import Firebase
 class homeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
-
+    
+    //ナビとセルの構築
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Home"
@@ -17,15 +18,20 @@ class homeController: UICollectionViewController, UICollectionViewDelegateFlowLa
         collectionView?.register(VideoCell.self, forCellWithReuseIdentifier: "cellId")
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //セッションの数の設定
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return 10
     }
+    
+    //セルの接続
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath as IndexPath)
     
         return cell
     }
     
+    //セルのレイアウトサイズ設定
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
         return CGSize(width:view.frame.width,height:200)
@@ -41,9 +47,12 @@ class VideoCell: UICollectionViewCell {
         setupView()
     }
     
+    //部品開発
     let titleLabel : UILabel = {
         let label = UILabel()
         label.backgroundColor = UIColor.purple
+        label.text = "TEDxTokyo"
+        label.textColor = UIColor(red: 255, green: 255, blue: 255, alpha: 1.0)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -51,6 +60,9 @@ class VideoCell: UICollectionViewCell {
     let subtitle : UITextView = {
         let text = UITextView()
         text.backgroundColor = UIColor.red
+        text.text = "TEDxTokyoは世界の最先端の情報を日本で取り扱うメディアサービスです。"
+        text.font = UIFont.boldSystemFont(ofSize: 8)
+        text.textColor = UIColor(red: 255, green: 255, blue: 255, alpha: 1.0)
         text.translatesAutoresizingMaskIntoConstraints = false
         return text
     }()
@@ -59,7 +71,6 @@ class VideoCell: UICollectionViewCell {
         let imageView = UIImageView()
         imageView.backgroundColor = UIColor.blue
         imageView.image = UIImage(named: "images")
-        
         return imageView
     }()
     
@@ -77,13 +88,16 @@ class VideoCell: UICollectionViewCell {
         return view
     }()
     
+    //表示
     func setupView() {
+        
         addSubview(VideoImageView)
         addSubview(separatorView)
         addSubview(ProfileImage)
         addSubview(titleLabel)
         addSubview(subtitle)
         
+        //autolayout
         addConstrainsWithFormat(format: "H:|-16-[v0]-16-|", views: VideoImageView)
         addConstrainsWithFormat(format: "H:|-16-[v0(44)]", views: ProfileImage)
         addConstrainsWithFormat(format: "V:|-16-[v0]-8-[v1(44)]-16-[v2(1)]|", views: VideoImageView,ProfileImage,separatorView)
@@ -104,6 +118,7 @@ class VideoCell: UICollectionViewCell {
     }
 }
 
+//mainimageの表示
 extension UIView {
     func addConstrainsWithFormat(format: String, views: UIView...) {
         var viewsDictionary = [String:UIView]()
---
+++ collection/Controller/signin.swift
@@ -0,0 +1,13 @@
+//
+//  signin.swift
+//  collection
+//
+//  Created by 小嶋涼 on 2018/05/05.
+//  Copyright © 2018年 小嶋涼. All rights reserved.
+//
+
+import Firebase
+
+class signin : UIViewController {
+    
+}
